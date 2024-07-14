f37770cf01581ff4bd5ea90f71a7b66442ef5c9f
nguyentienvu123 <140578919+nguyentienvu123@users.noreply.github.com>
2024-07-14 11:29:25 -0500

    react ui practice

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useMemo } from "react";
+import './App.css';
+
+const data = [[1,1,1] , [0,1,1] , [1,0,1], [1,0,1] ,[1,0,1], [1,0,1] ,[1,0,1]]
+
+function BoxSelect({data}) {
+  const boxes = useMemo(() => data.flat(Infinity), [data])
+
+  return (
+    <div className="container">
+
+      {
+        boxes.map((box, index) => (
+          <div key={`${box}-${index}`} className="box" />
+        ))
+      }
+
+
+    </div>
+  );
+}
+
+function App(){
+  return <>
+    <BoxSelect data={data}/>
+  </>
+}
+export default App;
